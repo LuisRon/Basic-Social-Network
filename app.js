cd2eb2a4c1f3f0082eeb66f7f5bfd07653f6ef8d
Luis Ron <luisron2007@gmail.com>
2020-03-28 12:31:21 +0100

    adding cors

--- app.js
+++ app.js
@@ -6,11 +6,13 @@ const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const expressValidator = require('express-validator');
 const dotenv = require('dotenv');
+const fs = require('fs');
+const cors = require('cors');
 dotenv.config();
 
 //db
-mongoose.connect(process.env.MONGO_URI,{useNewUrlParser: true})
-.then(()=> console.log("Db connected"));
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true })
+    .then(() => console.log("Db connected"));
 
 mongoose.connection.on('error', err => {
     console.log(`DB connection error: ${err.message}`);
@@ -21,19 +23,33 @@ const postRoutes = require("./routes/post");
 const authRoutes = require("./routes/auth");
 const userRoutes = require("./routes/user");
 
+//apiDocs
+app.get('/', (req, res) => {
+    fs.readFile('docs/apiDocs.json', (err, data) => {
+        if (err) {
+            res.status(400).json({
+                error: err
+            })
+        }
+        const docs = JSON.parse(data);
+        res.json(docs);
+    });
+});
+
 //middleware
 app.use(morgan("dev"));
 app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(expressValidator());
+app.use(cors());
 app.use("/", postRoutes);
 app.use("/", authRoutes);
 app.use("/", userRoutes);
-app.use(function (err,req,res,next){
-    if(err.name === 'UnauthorizedError'){
-        res.status(401).json({message: 'Unauthorized'});
+app.use(function (err, req, res, next) {
+    if (err.name === 'UnauthorizedError') {
+        res.status(401).json({ message: 'Unauthorized' });
     }
 });
 
 const port = process.env.PORT || 8080;
-app.listen(port, ()=> {console.log(`A Node Js API is listening on port: ${port}`)});
+app.listen(port, () => { console.log(`A Node Js API is listening on port: ${port}`) });
--- routes/post.js
+++ routes/post.js
@@ -13,7 +13,7 @@ const { userById } = require('../controllers/user');
 const { createPostValidator } = require('../validator');
 const router = express.Router();
 
-router.get('/', getPosts);
+router.get('/posts', getPosts);
 router.post('/post/new/:userId', requireSignin, createPost, createPostValidator);
 router.get('/posts/by/:userId', requireSignin, postsByUser);
 router.delete('/post/:postId', requireSignin, isPoster, deletePost);
